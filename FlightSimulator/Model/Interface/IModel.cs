dbff7eed38931947dc5768cd2f9b1780c900aa55
shulman pc <neriyashul@gmail.com>
2019-04-11 18:56:32 +0300

    model

---
+++ FlightSimulator/Model/Interface/IModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace FlightSimulator.Model.Interface
+{
+    interface IModel : INotifyPropertyChanged
+    {
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        // server
+        void openServer(string ip, int port);
+        void closeSever();
+      
+        // clients
+        void connectClient(ITelnetClient c);
+        void disconnectClient();
+        void start();
+    }
+}
---
+++ FlightSimulator/Model/MyModel.cs
@@ -0,0 +1,56 @@
+﻿using FlightSimulator.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model
+{
+    class MyModel : IModel
+    {
+        volatile Boolean stop;
+        private ITelnetServer server;
+        private ITelnetClient client;
+
+
+        public MyModel(ITelnetServer server, ITelnetClient client)
+        {
+            this.server = server;
+            stop = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void closeSever()
+        {
+            stop = true;
+            client.disconnect();
+        }
+
+        public void connectClient(ITelnetClient c)
+        {
+            int port = ApplicationSettingsModel.Instance.FlightCommandPort;
+            client.connect("127.0.0.1", port);
+        }
+
+        public void disconnectClient()
+        {
+            this.client.disconnect();
+        }
+
+        public void openServer(string ip, int port)
+        {
+            this.server.start(ip, port);
+        }
+
+
+
+        public void start()
+        {   
+
+        }
+    }
+}
